5c0754b48021d292ddabf8faaf87c5609d08ffe6
MyndLens Deploy <myndlens-deploy@emergent>
2026-02-19 23:29:48 +0000

    auto-commit for 79d77e6c-2b55-47f6-b948-b5eb61eae142

--- frontend/src/digital-self/index.ts
+++ frontend/src/digital-self/index.ts
@@ -4,8 +4,8 @@
  * Import from here, not from individual files.
  */
 
-export { loadPKG, savePKG, upsertNode, upsertEdge, storeFact, registerPerson } from './pkg';
-export type { PKG, PKGNode, PKGEdge, NodeType, EdgeType } from './pkg';
+import { loadPKG, savePKG, upsertNode, upsertEdge, storeFact, registerPerson } from './pkg';
+import type { PKG } from './pkg';
 export { resolve, getAttribute, buildContextCapsule, getStats } from './subject-graph';
 export type { ResolvedEntity, ContextCapsule } from './subject-graph';
 export { deleteDigitalSelf, getDigitalSelfSize } from './kill-switch';
